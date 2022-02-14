6a3dfaca9a95f4885c4965e9a98dcaab7a89c368
elinapavlova <elina.pavlova.02@mail.ru>
2022-02-14 16:23:10 +0300

    Add administrators table

---
+++ TelegramBotForReddit.Database/Models/AdministratorModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace TelegramBotForReddit.Database.Models
+{
+    public class AdministratorModel
+    {
+        public Guid Id { get; set; }
+        public long UserId { get; set; }
+    }
+}
